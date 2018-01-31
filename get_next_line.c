0838a4c56d88b7a7009a7645a1fd91fd38953363
Michiel PAUW <mpauw@e3r8p19.42.fr>
2018-01-31 17:10:05 +0100

    Not pretty, but functioning

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mpauw <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 13:39:05 by mpauw             #+#    #+#             */
-/*   Updated: 2017/11/20 15:15:19 by mpauw            ###   ########.fr       */
+/*   Updated: 2017/11/27 16:38:07 by mpauw            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static t_fd_list	*get_buff(int fd, int *new_buff)
 		prev = current;
 		current = current->next;
 	}
-	if (!(current = (t_fd_list *)malloc(sizeof(t_fd_list))) \
+	if (!(current = (t_fd_list *)malloc(sizeof(t_fd_list)))
 			|| !(current->buff = (char*)malloc(sizeof(char) * BUFF_SIZE)))
 		return (NULL);
 	if (prev)
@@ -41,14 +41,14 @@ static t_fd_list	*get_buff(int fd, int *new_buff)
 	return (current);
 }
 
-static void	reallocate_mem(char **str, int *prev_mem)
+static void			reallocate_mem(char **str, int *prev_mem)
 {
 	char	*temp;
 	int		i;
 
 	i = 0;
 	temp = *str;
-	if (!(*str = (char *)malloc(sizeof(char) * (*prev_mem * 2))))
+	if (!(*str = (char *)malloc(sizeof(char) * (*prev_mem * 2 + 1))))
 		return ;
 	while (*(temp + i))
 	{
@@ -59,60 +59,74 @@ static void	reallocate_mem(char **str, int *prev_mem)
 	free(temp);
 }
 
-static int		fill_string(char **line, t_fd_list *current_fd, int *alloc_size)
+static int			fill_string(char **line, t_fd_list *cur_fd, int *to_alloc)
 {
 	int		len_line;
-	int		i;
 
 	len_line = ft_strlen(*line);
-	i = -1;
-	while (++i < BUFF_SIZE && i < current_fd->read_r)
+	while (cur_fd->rsf < BUFF_SIZE && cur_fd->rsf < cur_fd->read_r)
 	{
-		if (len_line + 1 >= *alloc_size)
+		if ((cur_fd->buff)[cur_fd->rsf] == '\n')
 		{
-			reallocate_mem(line, alloc_size);
-		}
-		if ((current_fd->buff)[i] == '\n')
-		{
-			(current_fd->buff)[i] = 0;
+			*(*line + len_line) = 0;
+			(cur_fd->buff)[cur_fd->rsf++] = 0;
 			return (1);
 		}
-		if ((current_fd->buff)[i])
+		if (len_line + 1 >= *to_alloc)
+			reallocate_mem(line, to_alloc);
+		if ((cur_fd->buff)[cur_fd->rsf])
 		{
-			*(*line + len_line++) = (current_fd->buff)[i];
+			*(*line + len_line) = (cur_fd->buff)[cur_fd->rsf];
+			len_line++;
 			*(*line + len_line) = 0;
-			(current_fd->buff)[i] = 0;
+			(cur_fd->buff)[cur_fd->rsf] = 0;
 		}
+		(cur_fd->rsf)++;
 	}
-	return (current_fd->eof = ((i == current_fd->read_r && current_fd->read_r < BUFF_SIZE) ? 1 : 2));
+	cur_fd->eof = (cur_fd->rsf == cur_fd->read_r
+			&& cur_fd->read_r < BUFF_SIZE) ? 1 : 2;
+	return (cur_fd->eof);
 }
 
-int				get_next_line(const int fd, char **line)
+static int			read_file(char **line, t_fd_list *cur_fd,
+		int to_alloc, int is_new_buff)
 {
-	int			alloc_size;
 	int			fill_status;
-	int			is_new_buff;
-	t_fd_list	*current_fd;
 
-	current_fd = get_buff(fd, &is_new_buff);
-	alloc_size = BUFF_SIZE;
-	if (!line || !(*line = (char *)malloc(sizeof(char) * alloc_size)) || !current_fd)
-		return (-1);
-	if (current_fd->eof == 1)
-		return (0);
 	**line = 0;
 	fill_status = 0;
-	while (current_fd && (fill_status == 0 || fill_status == 2))
+	while (cur_fd && (fill_status == 0 || fill_status == 2))
 	{
-		if (is_new_buff || fill_status == 2)
+		if (fill_status == 2 || cur_fd->rsf == cur_fd->read_r)
 		{
 			is_new_buff = 0;
-			if (0 >= ((current_fd->read_r = read(fd, current_fd->buff, BUFF_SIZE))) && !(**line))
-				return (current_fd->read_r);
-			else if (!current_fd->read_r)
+			cur_fd->rsf = 0;
+			if (0 >= ((cur_fd->read_r =
+							read(cur_fd->fd, cur_fd->buff, BUFF_SIZE)))
+					&& !(**line))
+				return (cur_fd->read_r);
+			else if (!cur_fd->read_r)
 				return (1);
 		}
-		fill_status = fill_string(line, current_fd, &alloc_size);
+		fill_status = fill_string(line, cur_fd, &to_alloc);
 	}
 	return (fill_status);
+}
+
+int					get_next_line(const int fd, char **line)
+{
+	int			to_alloc;
+	int			is_new_buff;
+	int			to_return;
+	t_fd_list	*cur_fd;
+
+	cur_fd = get_buff(fd, &is_new_buff);
+	to_alloc = BUFF_SIZE;
+	if (!line || !(*line = (char *)malloc(sizeof(char) * to_alloc + 1))
+			|| !cur_fd)
+		return (-1);
+	if (cur_fd->eof == 1)
+		return (0);
+	to_return = read_file(line, cur_fd, to_alloc, is_new_buff);
+	return (to_return);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,27 +6,28 @@
 /*   By: mpauw <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 08:40:27 by mpauw             #+#    #+#             */
-/*   Updated: 2017/11/20 14:00:55 by mpauw            ###   ########.fr       */
+/*   Updated: 2017/11/27 16:44:11 by mpauw            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define	GET_NEXT_LINE_H
-# define	BUFF_SIZE 4
+# define GET_NEXT_LINE_H
 
+# define BUFF_SIZE 11000
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft/libft.h"
 
-int	get_next_line(const int fd, char **line);
+int					get_next_line(const int fd, char **line);
 
 typedef struct		s_fd_list
 {
 	int					fd;
 	int					read_r;
 	int					eof;
+	int					rsf;
 	char				*buff;
 	struct s_fd_list	*next;
 }					t_fd_list;
